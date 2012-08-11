49676682d5dcaf702ceef8ad6e81160705bd2538
epmak.g@gmail.com <epmak.g@gmail.com@211ab2f2-ebb3-0387-235a-e88ff477c1c6>
2012-08-11 05:08:34 +0000

    editing last patch

--- trunk/distrib/_sysvol/_a/checkmail.php
+++ trunk/distrib/_sysvol/_a/checkmail.php
@@ -27,7 +27,8 @@ if (get_accesslvl()>49)
  {
    $n = checknum(substr($_GET["mid"],0,5));
    $array = $db->fetchrow("SELECT * FROM MWC_messages WHERE id='".$n."'");
-   $db->query("UPDATE MWC_messages SET isread='1' WHERE id='".$n."'");
+   $db->query("UPDATE MWC_messages SET isread='1' WHERE id='".$n."' or slave_id='".$n."'");
+   
    $content->set('|smail_Date|', @date("H:i d.m.Y",$array[5]));
    $content->set('|mnik|', $array[1]);
    $content->set('|msg|', $array[2]);
@@ -54,13 +55,19 @@ if (get_accesslvl()>49)
   $query = $db->query("SELECT * FROM MWC_messages WHERE slave_id=0");
   while($myrows = $db->fetchrow($query))
   {
+   
+   $num = $db->fetchrow("SELECT [memb___id] FROM [MWC_messages] WHERE ([isread] ='0' and [slave_id]=".$myrows[0].")or ([isread] ='0' and [id]=".$myrows[0].")");
    $content->set('|m_name|', $myrows[1]);
+
+   if (strlen($num[0])>0) $content->set('|m_let|', "<img src='imgs/letterZ.png' border='0'>");
+   //if (isset($num) && $num[0]>=0) $content->set('|m_let|', "<img src='imgs/letterZ.png' border='0'>");
+   else  $content->set('|m_let|', "-");
    $content->set('|m_date|', @date("H:i d.m.Y",$myrows[5]));
    $content->set('|id|', $myrows[0]);
    $content->out_content("_sysvol/_a/theme/checkmail_c.html");
    $n++;
   }
-  if ($n==0) echo "<tr><td colspan='3'><div style='text-align:center'>You haven't mails</div></td></tr>";
+  if ($n==0) echo "<tr><td colspan='4'><div style='text-align:center'>You haven't mails</div></td></tr>";
  }
 
 
