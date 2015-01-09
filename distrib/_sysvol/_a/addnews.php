41755da3a72eb840d047c0c463f3bd40484d43f2
epmak.g@gmail.com <epmak.g@gmail.com@211ab2f2-ebb3-0387-235a-e88ff477c1c6>
2015-01-09 11:43:33 +0000

    переписал систему новостей (не добавлял пагинатора и прочей мути)

--- distrib/_sysvol/_a/addnews.php
+++ distrib/_sysvol/_a/addnews.php
@@ -1,112 +1,148 @@
-<?php 
-if (defined('inpanel'))
+<?php
+/**
+ * добавление/редактирование новостей
+ */
+error_reporting(E_ALL);
+if(!file_exists("_dat/news/newslist"))
 {
- ob_start();
- global $config;
- global $content;
- require "configs/news_cfg.php";
- $nforumlink = "none";
- if(!function_exists('get_magic_quotes_gpc'))
- {
-  function get_magic_quotes_gpc() {return false;}
- }
- $newsDB=file("_dat/news.dat");
- $content->out_content("_sysvol/_a/theme/addnews_h.html");
-	$content->set('|news_content|',"");
-  if (!$_GET["act"])
-  {
-   if (($_REQUEST["addnews"])&&(!empty($_POST['NewNews']))||(!empty($_POST['NewTitle']))) /*запись самих новостей*/
-   {
-    $mnews = bugsend(str_replace("\n","[br]",stripslashes($_POST["NewNews"])));
-    if ($news["newsf"]=="on") $nforumlink = htmlspecialchars(substr($_POST["flink"],0,$news["liklen"]));
-    else $nforumlink = "none";
-
-    if (!get_magic_quotes_gpc()) $mnews = addslashes($mnews); 
-			
-    $OpenNewsFile=fopen("_dat/news.dat",'a');
-    if (!get_magic_quotes_gpc()) $addn = addslashes(bugsend(htmlspecialchars($_POST["NewTitle"])));
-    else $addn = bugsend(htmlspecialchars($_POST["NewTitle"]));
-			
-    fwrite($OpenNewsFile,$addn."::".$mnews."::".$nforumlink."::".time().chr(13).chr(10));
-    fclose($OpenNewsFile);	
-    unset($mnews,$_REQUEST["addnews"],$addn);
-    header("Location:".$config["siteaddress"]."/control.php?page=addnews");
-   }
-  }	 
-  if ($_REQUEST["edited"])/*если нажата кнопка "редактировать"*/
-  {
-   $title = htmlspecialchars($_POST["NewTitle"]);
-   if ($news["newsf"]=="on") $nforumlink = htmlspecialchars(substr($_POST["flink"],0,$news["liklen"]));
-   else $nforumlink = "none";
-   $newsc = bugsend(str_replace("\n","[br]",stripslashes($_POST["NewNews"])));
-   $nom = checknum($_POST["new"]);
-   $nom=$_SESSION["newsnum"];
-   $newsF="_dat/news.dat";
-   if (!get_magic_quotes_gpc()) $newsc = addslashes($newsc); 
-   $newsDB[$nom]=$title."::".$newsc."::".$nforumlink."::".time().chr(13).chr(10);		
-   $fne = fopen($newsF,"w");
-   for ($i=0; $i<count($newsDB);$i++)
-   {fwrite($fne,$newsDB[$i]);}
-   fclose($fne);
-   $nforumlink = "none";
-   unset($title,$newsc,$_POST["NewNews"],$_REQUEST["edited"]);
-   header("Location:".$config["siteaddress"]."/control.php?page=addnews");
-  }
-  if ($_GET["act"]=="edit") /*поле редактирования*/
-  {
-			$nom = checknum(substr($_GET["new"],0,5));
-			$_SESSION["newsnum"] = $nom;
-			$openfile=file("_dat/news.dat");
-			list($title,$newsc,$nforumlink,$date)=split("::",$openfile[$nom]);
-			$content->set('|button_v|', news_editbtn." name='edited'"); 
-  }	
-  else
-	  $content->set('|button_v|', news_addbtn." name='addnews'");
-	
- if ($_GET["act"]=="del")
-	 {
-		$nom = validate($_GET["new"]);
-		$lnum = count($newsDB);
-		unset ($newsDB[$nom]);
-		$newsF="_dat/news.dat";	
-		$fdel = fopen($newsF,"w");
-		for ($i=0; $i<$lnum;$i++)
-		{
-			if (!$newsDB[$i]){ $i++; fwrite($fdel,$newsDB[$i]);}
-			else {fwrite($fdel,$newsDB[$i]);}
-		}
-		fclose($fdel);
-		unset($_SESSION["newsnum"]);
-		header("Location:".$config["siteaddress"]."/control.php?page=addnews");
-	 }
-	 
-
-	 $content->set('|content_title|', $title); 
-	 if (strlen($nforumlink)>4 ) $content->set('|checked|', "checked");
-	 else $content->set('|checked|', "");
-	 
-	 $content->set('|nforumlink|', $nforumlink);
-	 $content->set('|news_content|', unhtmlentities(str_replace("[br]","\n",stripslashes($newsc)))); 
-	 
-	 //if($_REQUEST["editnews"]) 
-	// else 
-
-	 $bdnews=file("_dat/news.dat");
-	 $kol=count($bdnews)-1;
-	 $content->out_content("_sysvol/_a/theme/addnews_c.html");
-	for ($i=$kol;$i>=0;$i--)
-	{
-	 list($title,$newsc,$nforumlink,$date)=split("::",$bdnews[$i]);
-
-	 $content->set('|i|', $i);
-	 $content->set('|date|', @date('d.m.Y',$date));
-	 $content->set('|titleZ|', bbcode(unhtmlentities($title)));
-	 $content->out_content("_sysvol/_a/theme/addnews_list.html");
-	}
-	$content->out_content("_sysvol/_a/theme/addnews_f.html");
- 
-
- $temp = ob_get_contents();
- ob_end_clean(); 
+ $h=fopen("_dat/news/newslist",'w'); //если нету файла-оглавления новостей, создаем
+ fclose($h);
 }
-else die();
+require "configs/news_cfg.php";
+
+$newsList = array();
+$listCont = file_get_contents("_dat/news/newslist");
+if(!empty($listCont))
+{
+ $newsList = unserialize($listCont);
+ /*
+  * если есть новости, ТО достаем список вида
+  * array[номер_файла]
+  * =>[title] тайтл новости
+  * =>[link] линк на форум
+  */
+}
+$nforumlink = "none";
+if(!function_exists('get_magic_quotes_gpc')) //если нету этой функции, делаем заглушку
+{
+    function get_magic_quotes_gpc()
+    {
+        return false;
+    }
+}
+
+$content->out("addnews_h.html");
+if(isset($_GET["act"]))
+{
+    switch((int)$_GET["act"])
+    {
+         //форма добавления новости
+         case 1:
+             $content->set("|content_title|","");
+             $content->set("|nforumlink|","");
+             $content->set("|news_content|","");
+             $content->set("|act|",2);
+             $content->set("|new|",0);
+             $content->set('|button_v|'," name='addnews'");
+             $content->out("addnews_c.html");
+             break;
+         //нажата кнопка добавления новостей
+         case 2:
+             if(isset($_REQUEST["addnews"]))
+             {
+                 $title = $_POST["NewTitle"];
+                 $news = $_POST["NewNews"];
+                 $flink = (isset($_POST["flink"])) ? $_POST["flink"] : "none";
+                 $cnt = count($newsList);
+                 if($cnt>0)
+                 {
+                     $fname = count($newsList);
+                 }
+                 else
+                 {
+                     $fname = 0;
+                 }
+
+                 $newsList[$cnt] = array("title" => $title, "link" => $flink);
+                 $h=fopen("_dat/news/newslist",'w');
+                 fwrite($h,serialize($newsList));
+                 fclose($h);
+
+                 $h=fopen("_dat/news/$fname",'w');
+                 fwrite($h,$news);
+                 fclose($h);
+
+                 header("Location:".$config["siteaddress"]."/control.php?page=addnews");
+                 die();
+             }
+             break;
+         //форма редактирования новости
+         case 3:
+             if(isset($_GET["new"]))
+             {
+                 $n = (int)$_GET["new"];
+                 if(isset($newsList[$n]) && file_exists("_dat/news/$n"))
+                 {
+                     $content->set("|content_title|",valid::decode($newsList[$n]["title"]));
+                     $link = valid::decode($newsList[$n]["link"]);
+                     if($link !="none")
+                         $content->set("|nforumlink|",$link);
+                     else
+                         $content->set("|nforumlink|","");
+
+                     $content->set("|news_content|",valid::decode(file_get_contents("_dat/news/$n")));
+                     $content->set("|act|",4);
+                     $content->set("|new|",$n);
+                     $content->set('|button_v|'," name='edited'");
+                     $content->out("addnews_c.html");
+                 }
+                 else
+                     header("Location:".$config["siteaddress"]."/control.php?page=addnews");
+             }
+            break;
+         //редактируем и сохраняем новость
+         case 4:
+             if(isset($_REQUEST["edited"]) && (isset($_GET["new"]) || $_GET["new"] == "0") )
+             {
+                 $n = (int)$_GET["new"];
+                 $title = $_POST["NewTitle"];
+                 $news = $_POST["NewNews"];
+                 $flink = (isset($_POST["flink"])) ? $_POST["flink"] : "none";
+                 $newsList[$n] = array("title" => $title, "link" => $flink);
+                 $h=fopen("_dat/news/newslist",'w');
+                 fwrite($h,serialize($newsList));
+                 fclose($h);
+
+                 $h=fopen("_dat/news/$n",'w');
+                 fwrite($h,$news);
+                 fclose($h);
+
+                 header("Location:".$config["siteaddress"]."/control.php?page=addnews");
+                 die();
+             }
+            break;
+            //удалить новость
+            case 5:
+                if(isset($_GET["new"]) || $_GET["new"] == "0")
+                {
+                    $n = (int)$_GET["new"];
+                    unset($newsList[$n]);
+
+                    $h=fopen("_dat/news/newslist",'w');
+                    fwrite($h,serialize($newsList));
+                    fclose($h);
+                    unlink("_dat/news/$n");
+                }
+                break;
+    }
+}
+
+//region список новостей
+$content->out("addnews_newtitle.html");
+foreach ($newsList as $id=>$news_title) {
+ $content->set('|i|', $id);
+ $content->set('|titleZ|', unhtmlentities($news_title["title"]));
+ $content->out("addnews_list.html");
+}
+$content->out("addnews_f.html");
+//endregion
