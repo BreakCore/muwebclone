49cd34c3975d48e3e96c12ca3336b8dce44c3ee8
epmak.g@gmail.com <epmak.g@gmail.com@211ab2f2-ebb3-0387-235a-e88ff477c1c6>
2015-02-07 07:30:22 +0000

    добавил визивинг для новостей добавил тултип для вещей разработка класса показа вещей

--- trunk/distrib/webshop.php
+++ trunk/distrib/webshop.php
@@ -13,10 +13,13 @@ require_once "_sysvol/fsql.php";
 require_once 'opt.php';
 require_once "_sysvol/engine.php";
 require_once '_sysvol/them.php';
+require_once "_sysvol/itemShow.php";
 
 $valid = new valid();
 $db = new connect ($config["ctype"], $config["db_host"], $config["db_name"], $config["db_user"], $config["db_upwd"]);
 $content = new content($config["siteaddress"],"site",substr($_SESSION["mwclang"],0,3),0,$config["theme"]);
+require_once "_sysvol/rItem.php";
+$iObj = new rItem();
 
 if(isset($_GET["act"]))
 {
@@ -27,8 +30,15 @@ if(isset($_GET["act"]))
             $q = $db->query("SELECT TOP 100 * FROM MWC_WEBSHOP");
             while ($r = $q->FetchRow())
             {
-                debug($r);
+                //debug($r);
+                $content->add_dict($r);
+                $content->out("wshop_c.html");
             }
+            break;
+        //отображение инфы по вещи
+        case 2:
+            $itm = $iObj->read($_GET["itmh"]);
+            itemShow::show($itm);
             break;
         default:
             die();
