b4dfc9a9af7849689d9477d24dfafed7bbc21222
epmak.g@gmail.com <epmak.g@gmail.com@211ab2f2-ebb3-0387-235a-e88ff477c1c6>
2015-02-09 09:55:45 +0000

    пилим класс отображения

--- trunk/distrib/_sysvol/itemShow.php
+++ trunk/distrib/_sysvol/itemShow.php
@@ -10,25 +10,88 @@ class itemShow
     public static function show($rItem)
     {
         //debug($rItem);
-
+        $class= "item";
         if(isset($rItem["exc"]))
+        {
             $rItem["name"] = "Excellent ".$rItem["name"];
+            $class = "exc";
+        }
         if(isset($rItem["anc"]))
+        {
             $rItem["name"] = "Ancient ".$rItem["name"];
-        if(isset($rItem["level"]) && $rItem["level"]>0)
-            $rItem["name"].=" +".$rItem["level"];
+            if($class=="exc")
+                $class = "excanc";
+            else
+                $class = "anc";
+        }
 
-        $output = "<p>{$rItem["name"]}</p>";
+        if(isset($rItem["level"]) && $rItem["level"]>0)
+        {
+            $rItem["name"].=" +".$rItem["level"];
+            if($class == "item")
+                $class = "item".$rItem["level"];
+        }
+
+        if(isset($rItem["isSkill"]) || isset($rItem["lifeOpt"]))
+        {
+            $class = "itemwithoption";
+        }
+        $output[$class] = $rItem["name"];
 
         $img = self::getImg($rItem);
         if($img)
-            $output .= "<p>$img</p>";
+            $output["image"]= "$img";
+
+        if(isset($rItem["speed"]))
+            $output["speed"] = $rItem["speed"];
+
+        if(isset($rItem["str"]))
+        {
+            $output["str"]= "Strength Requirement: ".$rItem["str"];
+        }
+        if(isset($rItem["str"]))
+        {
+            $output["agi"]= "Agility Requirement: ".$rItem["agi"];
+        }
+        if(isset($rItem["ene"]))
+        {
+            $output["ene"]= "Energy Requirement: ".$rItem["ene"];
+        }
+        if(isset($rItem["vit"]))
+        {
+            $output["vit"]= "Vitality Requirement: ".$rItem["vit"];
+        }
+        if(isset($rItem["cmd"]))
+        {
+            $output["cmd"]= "Command Requirement: ".$rItem["cmd"];
+        }
+
+        if(isset($rItem["exc"]) && is_array($rItem["exc"]))
+        {
+            $output["exc"] = $rItem["exc"];
+        }
+        if(isset($rItem["pvp"]))
+        {
+            $output["pvp"] = $rItem["pvp"];
+        }
+
+        if(isset($rItem["anc"]))
+        {
+            $output["anc"] = $rItem["anc"];
+        }
+
 
         if(isset($rItem["equipment"]))
-            $output .= "<p>{$rItem["equipment"]}</p>";
+            $output["equipment"]= $rItem["equipment"];
+        if(isset($rItem["isLuck"]))
+            $output["luck"] = self::$luck;
+        if(isset($rItem["skillname"]))
+            $output["skillname"] = $rItem["skillname"];
 
+        if(isset($rItem["harmony"]))
+            $output["harmony"] = $rItem["harmony"];
 
-        echo $output;
+        return $output;
     }
 
     static public function getImg($rItem)
